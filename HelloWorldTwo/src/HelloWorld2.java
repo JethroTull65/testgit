28cc1bcfd7ba1549f27328b77ebf2ffe828da0a6
Jethro Tull65 <urquell65@live.com>
2016-02-23 13:57:18 -0800

    clean up main

--- HelloWorldTwo/src/HelloWorld2.java
+++ HelloWorldTwo/src/HelloWorld2.java
@@ -1,26 +1,13 @@
 /**
- * 
- */
-
-/**
  * @author gerald.heidenreich
  *
  */
 public class HelloWorld2 {
 
-	/**
-	 * 
-	 */
-	public HelloWorld2() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
         System.out.println("Hello, World");
+
 	}
 
 }
